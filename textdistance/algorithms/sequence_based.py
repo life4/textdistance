be3ef97ced9fed3dfab85dc42b45049230125277
Gram <master_fess@mail.ru>
2017-12-29 15:10:50 +0500

    +LCSSeq

--- textdistance/algorithms/sequence_based.py
+++ textdistance/algorithms/sequence_based.py
@@ -12,8 +12,26 @@ __all__ = ['lcsseq']
 class LCSSeq(_BaseSimilarity):
     """cosine similarity (Ochiai coefficient)
     """
+    def __init__(self, qval=1, sim_test=None, empty=''):
+        self.qval = qval
+        self.empty = empty
+        self.sim_test = sim_test or self._ident
+
     def __call__(self, *sequences):
-        pass
+        if not all(sequences):
+            return self.empty
+        if self.sim_test(*[s[-1] for s in sequences]):
+            c = sequences[0][-1]
+            sequences = [s[:-1] for s in sequences]
+            return self(*sequences) + c
+        m = self.empty
+        for i, s in enumerate(sequences):
+            ss = sequences[:i] + s[:-1] + sequences[i + 1:]
+            m = max([self(ss), m], key=len)
+        return m
+
+    def similarity(self, *sequences):
+        return len(self(*sequences))
 
 
-lcsseq =LCSSeq()
+lcsseq = LCSSeq()
