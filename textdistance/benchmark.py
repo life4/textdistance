624e34deffc91d90aa426ecbcc6de8b141c202a8
Gram <master_fess@mail.ru>
2018-03-28 17:50:28 +0500

    benchmark result filtering and saving

--- textdistance/benchmark.py
+++ textdistance/benchmark.py
@@ -1,12 +1,13 @@
 import importlib
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from timeit import timeit
+import json
 
 from tabulate import tabulate
 
 from . import algorithms as textdistance
 from .constants import LIBRARIES_FILE
-from .labraries import LIBRARIES
+from .libraries import LIBRARIES
 
 
 # python3 -m textdistance.benchmark
@@ -58,24 +59,46 @@ class Benchmark(object):
                 object=getattr(textdistance, alg),
             )
 
+    @staticmethod
+    def filter_benchmark(external, internal):
+        limits = {i.algorithm: i.time for i in internal}
+        return filter(lambda x: x.time < limits[x.algorithm], external)
+
+    @staticmethod
+    def get_table(data):
+        table = tabulate(
+            [tuple(i[:-1]) for i in data],
+            headers=['algorithm', 'library', 'function', 'time'],
+        )
+        table += '\nTotal: {} libs.\n\n'.format(len(data))
+        return table
+
+    @staticmethod
+    def save(libs):
+        data = defaultdict(list)
+        for lib in libs:
+            data[lib.algorithm].append('{}.{}'.format(lib.library, lib.function))
+        with open(LIBRARIES_FILE, 'w') as f:
+            json.dump(obj=data, fp=f, indent=2, sort_keys=True)
+
     @classmethod
     def run(cls):
         print('# Installed libraries:\n')
         installed = list(cls.get_installed())
-        print(tabulate(
-            [tuple(i[:-2]) for i in installed],
-            headers=['algorithm', 'library', 'function'],
-        ))
-        print('Total: {} libs.\n\n'.format(len(installed)))
+        print(cls.get_table(installed))
 
-        print('# Benchmarks:\n')
+        print('# Benchmarks (with textdistance):\n')
         benchmark = list(cls.get_external_benchmark(installed))
-        benchmark += list(cls.get_internal_benchmark())
-        print(tabulate(
-            [tuple(i[:-1]) for i in benchmark],
-            headers=['algorithm', 'library', 'function', 'time'],
-        ))
-        print('Total: {} libs.\n\n'.format(len(benchmark)))
+        benchmark_internal = list(cls.get_internal_benchmark())
+        benchmark += benchmark_internal
+        benchmark.sort(key=lambda x: (x.algorithm, x.time))
+        print(cls.get_table(benchmark))
+
+        print('# Faster than textdistance:\n')
+        benchmark = list(cls.filter_benchmark(benchmark, benchmark_internal))
+        print(cls.get_table(benchmark))
+
+        cls.save(benchmark)
 
 
 if __name__ == '__main__':
