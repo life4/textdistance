c5c913c99b7a3a7b03b4e1afadc02d5347b00597
orsinium <master_fess@mail.ru>
2018-03-29 23:27:02 +0500

    +main package  info. Close #8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name     = 'textdistance',
-    version  = '2.0.3',
+    version  = '2.0.4',
 
     author       = 'orsinium',
     author_email = 'master_fess@mail.ru',
--- textdistance/__init__.py
+++ textdistance/__init__.py
@@ -1,4 +1,21 @@
+"""
+TextDistance.
+Compute distance between sequences.
+30+ algorithms, pure python implementation, common interface. 
+"""
 
+# main package info
+__title__ = 'TextDistance'
+__version__ = '2.0.4'
+__author__ = 'Gram Orsinium'
+__license__ = 'LGPL 3.0'
+
+
+# version synonym
+VERSION = __version__
+
+
+# facade
 from .interfaces import distance    # noQA
 from .utils import *                # noQA
 from .algorithms import *           # noQA
