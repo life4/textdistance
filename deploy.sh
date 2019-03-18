02b7fc3bcc199f236872b48c7772ed64d0de6b8a
Gram <master_fess@mail.ru>
2019-03-18 11:55:01 +0300

    release

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+set -e
 pandoc --from=markdown --to=rst --output=README.rst README.md
 python3 setup.py sdist
 twine upload dist/*
--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ except TypeError:
 
 setup(
     name='textdistance',
-    version='4.1.1',
+    version='4.1.2',
 
     author='orsinium',
     author_email='master_fess@mail.ru',
