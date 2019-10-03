453dd558f8324608e603bb01fde69474d3bca3a5
Gram <master_fess@mail.ru>
2019-10-03 11:56:09 +0200

    test LCSStr

--- tests/sequence_based.py
+++ tests/sequence_based.py
@@ -20,3 +20,28 @@ class LCSSeqTest(unittest.TestCase):
 
         self.assertEqual(self.alg('a' * 80, 'a' * 80), 'a' * 80)
         self.assertEqual(self.alg('a' * 80, 'b' * 80), '')
+
+
+class LCSStrTest(unittest.TestCase):
+    alg = textdistance.lcsstr
+
+    def test_common(self):
+        # prefix
+        self.assertEqual(self.alg('ab', 'abcd'), 'ab')
+        self.assertEqual(self.alg('abcd', 'ab'), 'ab')
+
+        # middle
+        self.assertEqual(self.alg('abcd', 'bc'), 'bc')
+        self.assertEqual(self.alg('bc', 'abcd'), 'bc')
+
+        # suffix
+        self.assertEqual(self.alg('abcd', 'cd'), 'cd')
+        self.assertEqual(self.alg('abcd', 'cd'), 'cd')
+
+        # no
+        self.assertEqual(self.alg('abcd', 'ef'), '')
+        self.assertEqual(self.alg('ef', 'abcd'), '')
+
+        # long
+        self.assertEqual(self.alg('MYTEST' * 100, 'TEST'), 'TEST')
+        self.assertEqual(self.alg('TEST', 'MYTEST' * 100), 'TEST')
